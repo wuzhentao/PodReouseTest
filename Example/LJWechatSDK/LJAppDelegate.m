0bef7ccbc4d34c8942a4a48d9283e6079606e9f2
夕才 <zhentao.wzt@alibaba-inc.com>
2017-11-14 15:16:22 +0800

    fix、

--- Example/LJWechatSDK/LJAppDelegate.m
+++ Example/LJWechatSDK/LJAppDelegate.m
@@ -11,7 +11,7 @@
 @implementation LJAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{    //fix bug1111111
+{    //fix bug111111122222
     // Override point for customization after application launch.
     return YES;
 }
